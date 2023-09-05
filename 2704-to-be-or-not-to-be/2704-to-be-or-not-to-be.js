8d853ce830a6cbe2c3687df6e647c32985bd80e1
Shreyansh Kumar <shreyanshkumar058@gmail.com>
2023-09-05 22:31:19 +0530

    Time: 43 ms (95.01%), Space: 42.2 MB (17.21%) - LeetHub

---
+++ 2704-to-be-or-not-to-be/2704-to-be-or-not-to-be.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function(val) {
+  return {
+        toBe:(b)=>{
+            if (val!==b) throw new Error("Not Equal");
+            else return true
+        },
+        notToBe:(c) =>{
+            if (val===c) throw new Error("Equal");
+            else return true
+        }
+
+    }
+};
+
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
